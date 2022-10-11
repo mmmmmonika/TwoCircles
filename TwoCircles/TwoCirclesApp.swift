c1658365b0e3c7f81b6c9c98461e8946cdf73e75
Monika Piesyk (External) <mpiesyk@blackberry.com>
2022-10-11 23:11:56 +0200

    Initial Commit

---
+++ TwoCircles/TwoCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoCirclesApp.swift
+//  TwoCircles
+//
+//  Created by Monika Piesyk on 11/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TwoCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
